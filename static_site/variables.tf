9e0fd35943e11a257a3861a9444853c2241a23e1
Zhen Yang <markt0815@icloud.com>
2025-09-02 04:49:26 -0400

    fix

--- static_site/variables.tf
+++ static_site/variables.tf
@@ -1,20 +1,20 @@
 variable "domain_name"    { type = string }
 variable "hosted_zone_id" { type = string }
 
-variable "github_owner"   { type = string }  # e.g., "gitzhen0"
-variable "github_repo"    { type = string }  # e.g., "microshop_frontend"
-variable "github_branch" {
+variable "frontend_github_owner"   { type = string }  # e.g., "gitzhen0"
+variable "frontend_github_repo"    { type = string }  # e.g., "microshop_frontend"
+variable "frontend_github_branch" {
   type    = string
   default = "main"
 }
 
 # 账户里若已存在 GitHub OIDC Provider，可设 false 复用
-variable "create_github_oidc_provider" { 
+variable "frontend_create_github_oidc_provider" { 
     type = bool 
     default = true 
 }
 
-variable "force_destroy_bucket" {
+variable "frontend_force_destroy_bucket" {
     type = bool
     default = false
 }
