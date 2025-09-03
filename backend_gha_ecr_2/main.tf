baea18af0e858af7d67f0b3cb45a5dee08caffec
Zhen Yang <markt0815@icloud.com>
2025-09-03 14:14:42 -0400

    updateforcedelete

--- backend_gha_ecr_2/main.tf
+++ backend_gha_ecr_2/main.tf
@@ -59,6 +59,7 @@ resource "aws_ecr_repository" "svc" {
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration { scan_on_push = true }
   encryption_configuration { encryption_type = "AES256" }
+  force_delete = true
 }
 
 # ---- 允许此角色 Push / Pull 到上述 ECR ----
